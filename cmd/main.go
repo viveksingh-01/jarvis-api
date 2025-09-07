129c0d78e495c2cce2f03e518f2bd54d994eb637
Vivek Singh <viveksingh9301@gmail.com>
2025-09-07 12:24:07 +0530

    Add godotenv dependency and implement environment variable loading in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	loadEnvVariables()
+}
 
 func main() {
 	fmt.Println("Welcome to JARVIS API")
@@ -17,4 +22,12 @@ func main() {
 	port := "9090"
 	log.Println("Server started at port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func loadEnvVariables() {
+	if os.Getenv("PRODUCTION") == "" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading the .env file.")
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/viveksingh-01/jarvis-api
 
 go 1.24.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
