43c832a530c35787e3599226784a6082169c1df8
Vivek Singh <viveksingh9301@gmail.com>
2025-09-07 10:03:54 +0530

    Add Gorilla Mux dependency; modify main.go to use Mux for routing

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Welcome to JARVIS API")
 
+	// Instantiate Gorilla Mux for routing
+	r := mux.NewRouter()
+
 	port := "9090"
 	log.Println("Server started at port:", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/viveksingh-01/jarvis-api
 
 go 1.24.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
