419ead0fb3add4abf87548106c29de1a41d211de
Vivek Singh <viveksingh9301@gmail.com>
2025-09-13 09:32:43 +0530

    Implement request validation and error handling in conversation handler

--- handlers/conversation.go
+++ handlers/conversation.go
@@ -3,11 +3,22 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/viveksingh-01/jarvis-api/utils"
 	"google.golang.org/genai"
 )
 
 var Client *genai.Client
 
 func HandleConversation(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	if r.Method != http.MethodPost {
+		utils.SendErrorResponse(w, http.StatusMethodNotAllowed, utils.ErrorResponse{
+			Error: "Invalid request method, please use POST method",
+		})
+	}
+	if r.Body == nil {
+		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
+			Error: "Request body cannot be empty",
+		})
+		defer r.Body.Close()
+	}
 }
