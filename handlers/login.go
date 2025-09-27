133deba5fcdeb9ed3a379d9d6467a5c18f657432
Vivek Singh <viveksingh9301@gmail.com>
2025-09-27 22:24:46 +0530

    Enhance login handler to validate request method and body

--- handlers/login.go
+++ handlers/login.go
@@ -1,7 +1,18 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/viveksingh-01/jarvis-api/utils"
+)
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	if !utils.ValidatePostRequestMethod(w, r) {
+		return
+	}
+	if !utils.ValidateRequestBody(w, r) {
+		return
+	}
+
 	// TODO
 }
