d022b602d3954e22b348d379a9b110f155fb188e
Vivek Singh <viveksingh9301@gmail.com>
2025-09-27 22:26:38 +0530

    Implement JSON decoding for login request body in the login handler

--- handlers/login.go
+++ handlers/login.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/viveksingh-01/jarvis-api/models"
 	"github.com/viveksingh-01/jarvis-api/utils"
 )
 
@@ -12,6 +14,13 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 	if !utils.ValidateRequestBody(w, r) {
 		return
+	}
+
+	var req models.LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
+			Error: "Invalid request body",
+		})
 	}
 
 	// TODO
