6c27aae6842fd46fbf6b9b89a230a4a2d77f7180
Vivek Singh <viveksingh9301@gmail.com>
2025-10-30 23:34:21 +0530

    Add user existence check in login handler to improve error handling

--- handlers/login.go
+++ handlers/login.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 
+	"github.com/viveksingh-01/jarvis-api/database"
 	"github.com/viveksingh-01/jarvis-api/models"
 	"github.com/viveksingh-01/jarvis-api/utils"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +26,17 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
 			Error: "Invalid request body",
 		})
+	}
+
+	var user models.User
+	// Check if the user exists
+	err := database.UserCollection.FindOne(context.TODO(), bson.M{"email": req.Email}).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		log.Println("User doesn't exist")
+		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
+			Error: "User doesn't exist, please create an account.",
+		})
+		return
 	}
 
 	// TODO
