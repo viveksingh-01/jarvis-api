2326b78abb07dafa66c02bfd68573e29da8fd672
Vivek Singh <viveksingh9301@gmail.com>
2025-11-01 16:11:12 +0530

    Add validation in registration handler to ensure proper request method and body

--- handlers/register.go
+++ handlers/register.go
@@ -1,7 +1,17 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/viveksingh-01/jarvis-api/utils"
+)
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	if !utils.ValidatePostRequestMethod(w, r) {
+		return
+	}
+	if !utils.ValidateRequestBody(w, r) {
+		return
+	}
 	// TODO
 }
