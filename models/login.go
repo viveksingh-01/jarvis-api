432d4935c6bc6a41c38719c60c0570bbc98aad5f
Vivek Singh <viveksingh9301@gmail.com>
2025-11-02 23:21:22 +0530

    Add RegisterRequest model struct for user registration

--- models/login.go
+++ models/login.go
@@ -4,3 +4,9 @@ type LoginRequest struct {
 	Email    string `bson:"email"`
 	Password string `bson:"password"`
 }
+
+type RegisterRequest struct {
+	Email    string `bson:"email"`
+	Name     string `bson:"name"`
+	Password string `bson:"password"`
+}
