9a7e66e3606ce539fb25b316975f71e69e54a90b
Vivek Singh <viveksingh9301@gmail.com>
2025-11-02 23:26:25 +0530

    Add JSON decoding and error handling for registration request body

--- handlers/register.go
+++ handlers/register.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/viveksingh-01/jarvis-api/models"
 	"github.com/viveksingh-01/jarvis-api/utils"
 )
 
@@ -13,5 +15,12 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	if !utils.ValidateRequestBody(w, r) {
 		return
 	}
+	var req models.RegisterRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
+			Error: "Invalid request body",
+		})
+	}
+
 	// TODO
 }
