4b1e7e2ea54904d032914f539f832f6ad1cb4d46
Vivek Singh <viveksingh9301@gmail.com>
2025-11-04 22:03:58 +0530

    Add user existence check in registration handler to prevent duplicate email registrations

--- handlers/register.go
+++ handlers/register.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
+	"github.com/viveksingh-01/jarvis-api/database"
 	"github.com/viveksingh-01/jarvis-api/models"
 	"github.com/viveksingh-01/jarvis-api/utils"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +23,16 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
 			Error: "Invalid request body",
 		})
+	}
+
+	var user models.User
+	// Check if the user already exists based on the 'email'
+	err := database.UserCollection.FindOne(context.TODO(), bson.M{"email": req.Email}).Decode(&user)
+	if err == nil {
+		utils.SendErrorResponse(w, http.StatusBadRequest, utils.ErrorResponse{
+			Error: "The email is already registered.\n Please try logging in.",
+		})
+		return
 	}
 
 	// TODO
